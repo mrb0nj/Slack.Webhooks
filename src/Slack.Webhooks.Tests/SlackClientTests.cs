22f02d025a9dee4ffcb0c8cebd699b2b552d082d
Jonathan Channon <jonathan.channon@gmail.com>
2015-01-18 10:50:52 +0000

    tidied test name

--- src/Slack.Webhooks.Tests/SlackClientTests.cs
+++ src/Slack.Webhooks.Tests/SlackClientTests.cs
@@ -6,7 +6,7 @@ namespace Slack.Webhooks.Tests
     public class SlackClientTests
     {
         [Fact]
-        public void FactMethodName()
+        public void SlackClient_should_throw_exception_if_slack_url_not_given()
         {
             Assert.Throws<ArgumentException>(() => new SlackClient("no slack url passed in"));
         }
