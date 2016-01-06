2c1c2530972922640e393f9e28c0f423818a44cc
Jordan Hofker <jhofker@gmail.com>
2016-01-06 10:49:20 -0600

    Make internals visible to the testing project

--- src/Slack.Webhooks/Properties/AssemblyInfo.cs
+++ src/Slack.Webhooks/Properties/AssemblyInfo.cs
@@ -34,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.8.*")]
 [assembly: AssemblyFileVersion("0.1.8.0")]
+
+[assembly: InternalsVisibleTo("Slack.Webhooks.Tests")]
