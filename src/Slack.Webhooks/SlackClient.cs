9cf7dad44d31a54a33dc6c31d52b612efc87853f
DESKTOP-K4V8LV8\Piers MacDonald <piers@gofetch.ca>
2017-05-05 15:26:42 -0700

    - convert project to .NET Standard for maximum compatibility against projects - prioritize async access while supporting sync method calls - minimize dependencies (remove servicestack & restsharp in favour of JSON.NET and HTTPClient) - convert test project to .NET core for cross platform compatibility.

--- src/Slack.Webhooks/SlackClient.cs
+++ src/Slack.Webhooks/SlackClient.cs
@@ -1,132 +1,113 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Reflection;
-using RestSharp;
-using ServiceStack.Text;
-
-#if NET40
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
-using RestSharp.Extensions;
-#endif
 
 namespace Slack.Webhooks
 {
-    public class SlackClient
+
+    /// <summary>
+    /// Default ISlackClient implementation
+    /// </summary>
+    public class SlackClient : ISlackClient
     {
-        private readonly RestClient _restClient;
+        private readonly HttpClient _httpClient;
         private readonly Uri _webhookUri;
+        private JsonSerializerSettings _jsonSettings;
 
         private const string VALID_HOST = "hooks.slack.com";
         private const string POST_SUCCESS = "ok";
 
-        /// <summary>
-        /// Returns the RestClient's current Timeout value.
-        /// </summary>
-        internal int TimeoutMs { get { return _restClient.Timeout; } }
+        public double TimeoutMs => _httpClient.Timeout.TotalMilliseconds;
 
-        public SlackClient(string webhookUrl, int timeoutSeconds = 100)
+        public SlackClient(string webhookUrl, double timeoutSeconds = 100)
         {
             if (!Uri.TryCreate(webhookUrl, UriKind.Absolute, out _webhookUri))
+            {
                 throw new ArgumentException("Please enter a valid Slack webhook url");
+            }
 
             if (_webhookUri.Host != VALID_HOST)
+            {
                 throw new ArgumentException("Please enter a valid Slack webhook url");
-
-            var baseUrl = _webhookUri.GetLeftPart(UriPartial.Authority);
-            _restClient = new RestClient(baseUrl);
-            _restClient.Timeout = timeoutSeconds * 1000;
-        }
-
-        private void SetPropertyConvention(JsConfigScope scope)
-        {
-            var assembly = Assembly.GetAssembly(typeof(JsConfig));
-            var jsConfig = assembly.GetType("ServiceStack.Text.JsConfig");
-            var conventionEnum = assembly.GetType("ServiceStack.Text.JsonPropertyConvention") ??
-                           assembly.GetType("ServiceStack.Text.PropertyConvention");
-            var propertyConvention = jsConfig.GetProperty("PropertyConvention");
-
-            var lenient = conventionEnum.GetField("Lenient");
-
-            var enumValue = Enum.ToObject(conventionEnum, (int)lenient.GetValue(conventionEnum));
-            propertyConvention.SetValue(scope, enumValue, null);
-        }
-
-        public virtual bool Post(SlackMessage slackMessage)
-        {
-            var request = new RestRequest(_webhookUri.PathAndQuery, Method.POST);
-            using (var scope = JsConfig.BeginScope())
-            {
-                scope.EmitLowercaseUnderscoreNames = true;
-                scope.IncludeNullValues = false;
-                SetPropertyConvention(scope);
-
-                request.AddParameter("payload", JsonSerializer.SerializeToString(slackMessage));
-
-                try
-                {
-                    var response = _restClient.Execute(request);
-                    return response.StatusCode == HttpStatusCode.OK && response.Content == POST_SUCCESS;
-                }
-                catch (Exception)
-                {
-                    return false;
-                }
-            }
-        }
-
-        public bool PostToChannels(SlackMessage message, IEnumerable<string> channels)
-        {
-            return channels.DefaultIfEmpty(message.Channel)
-                    .Select(message.Clone)
-                    .Select(Post).All(r => r);
-        }
-
-#if NET40
-        public IEnumerable<Task<IRestResponse>> PostToChannelsAsync(SlackMessage message, IEnumerable<string> channels)
-        {
-            return channels.DefaultIfEmpty(message.Channel)
-                                .Select(message.Clone)
-                                .Select(PostAsync);
-        }
-
-        public Task<IRestResponse> PostAsync(SlackMessage slackMessage)
-        {
-            var request = new RestRequest(_webhookUri.PathAndQuery, Method.POST);
-            using (var scope = JsConfig.BeginScope())
-            {
-                scope.EmitLowercaseUnderscoreNames = true;
-                scope.IncludeNullValues = false;
-                SetPropertyConvention(scope);
-
-                request.AddParameter("payload", JsonSerializer.SerializeToString(slackMessage));
             }
 
-            return ExecuteTaskAsync(request);
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = _webhookUri;
+            _httpClient.Timeout = TimeSpan.FromSeconds(timeoutSeconds);
+
+            _jsonSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                DefaultValueHandling = DefaultValueHandling.Ignore
+            };
         }
 
-        private Task<IRestResponse> ExecuteTaskAsync(IRestRequest request)
+        /// <summary>
+        /// Post a message 
+        /// </summary>
+        /// <param name="message">Message to be posted</param>
+        /// <returns>True if successful, false otherwise.</returns>
+        public virtual async Task<bool> PostToChannelAsync(SlackMessage slackMessage)
         {
-            var taskCompletionSource = new TaskCompletionSource<IRestResponse>();
+            var stringContent = new StringContent(JsonConvert.SerializeObject(slackMessage, _jsonSettings), Encoding.UTF8, "application/json");
             try
             {
-                _restClient.ExecuteAsync(request, (response, _) =>
-                {
-                    if (response.ErrorException != null)
-                        taskCompletionSource.TrySetException(response.ErrorException);
-                    else if (response.ResponseStatus != ResponseStatus.Completed)
-                        taskCompletionSource.TrySetException(response.ResponseStatus.ToWebException());
-                    else
-                        taskCompletionSource.TrySetResult(response);
-                });
+                var response = await _httpClient.PostAsync(_webhookUri.PathAndQuery, stringContent);
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return response.StatusCode == HttpStatusCode.OK && responseContent == POST_SUCCESS;
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                taskCompletionSource.TrySetException(ex);
-            }
-            return taskCompletionSource.Task;
+                return false;
+            }          
         }
-#endif
+
+        /// <summary>
+        /// Legacy method to maintain backward compatibility.
+        /// </summary>
+        [Obsolete("This method is deprecated in favour of the PostToChannelAsync one.")]
+        public bool Post(SlackMessage slackMessage)
+        {
+            return PostToChannelAsync(slackMessage).Result;
+        }
+
+        /// <summary>
+        /// Syncronous version of PostToChannelAsync
+        /// </summary>
+        public bool PostToChannel(SlackMessage slackMessage)
+        {
+            return PostToChannelAsync(slackMessage).Result;
+        }
+
+        /// <summary>
+        /// Post a message to multiple channels
+        /// </summary>
+        /// <param name="message">Message to be posted</param>
+        /// <param name="channels">Collection of channel names to post message to.</param>
+        /// <returns>True if successful, false otherwise.</returns>
+        public async Task<bool> PostToChannelsAsync(SlackMessage message, IEnumerable<string> channels)
+        {
+            var channelTasks = channels
+                .Select(message.Clone)
+                .Select(PostToChannelAsync);
+
+            var results = await Task.WhenAll(channelTasks);
+
+            //return true only if all sub-results are true
+            return results.All(result => result) ? true : false;
+        }
+
+        /// <summary>
+        /// Syncronous version of PostToChannelsAsync
+        /// </summary>
+        public bool PostToChannels(SlackMessage message, IEnumerable<string> channels)
+        {
+            return PostToChannelsAsync(message, channels).Result;
+        }
     }
 }
--- src/Slack.Webhooks/SlackMessage.cs
+++ src/Slack.Webhooks/SlackMessage.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 
@@ -9,45 +10,62 @@ namespace Slack.Webhooks
     public class SlackMessage
     {
         private bool _markdown = true;
+
         /// <summary>
         /// This is the text that will be posted to the channel
-        /// </summary>
+        /// </summary>        
+        [JsonProperty("text")]
         public string Text { get; set; }
+
         /// <summary>
         /// Optional override of destination channel
         /// </summary>
+        [JsonProperty("channel")]
         public string Channel { get; set; }
+
         /// <summary>
         /// Optional override of the username that is displayed
-        /// </summary>
+        [JsonProperty("username")]
         public string Username { get; set; }
+
         /// <summary>
         /// Optional emoji displayed with the message
         /// </summary>
+        [JsonProperty("icon_emoji")]
         public string IconEmoji { get; set; }
+
         /// <summary>
         /// Optional url for icon displayed with the message
         /// </summary>
+        [JsonProperty("icon_url")]
         public Uri IconUrl { get; set; }
+
         /// <summary>
         /// Optional override markdown mode. Default: true
         /// </summary>
+        [JsonIgnore]
         public bool Mrkdwn
         {
             get { return _markdown; }
             set { _markdown = value; }
         }
+
         /// <summary>
         /// Enable linkification of channel and usernames
         /// </summary>
+        [JsonProperty("link_names"), JsonIgnore()]
         public bool LinkNames { get; set; }
+
         /// <summary>
         /// Parse mode <see cref="ParseMode"/>
         /// </summary>
+        [JsonProperty("parse")]
         public string Parse { get; set; }
+
         /// <summary>
         /// Optional attachment collection
         /// </summary>
+        [JsonProperty("attachments")]
         public List<SlackAttachment> Attachments { get; set; }
 
 		public SlackMessage Clone(string newChannel = null)
