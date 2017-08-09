6be738e48a65e04f2e54f0b4af2aa6a3713246ac
Benn <benn@redrabbits.co.uk>
2017-08-09 16:54:39 +0100

    WIP

---
+++ src/Slack.Webhooks/SlackAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Slack.Webhooks
+{
+    /// <summary>
+    /// Slack attachment action. An attachment can have zero or more actions.
+    /// </summary>
+    public class SlackAction
+    {
+        private const string _type = "button";
+        public string Name { get; set; }
+        public string Type { get { return _type; } }
+        public string Text { get; set; }
+        public string Value { get; set; }
+        public SlackConfirmAction Confirm { get; set; }
+    }
+}
---
+++ src/Slack.Webhooks/SlackConfirmAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Slack.Webhooks
+{
+    public class SlackConfirmAction
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string OkText { get; set; }
+        public string DismissText { get; set; }
+    }
+}
