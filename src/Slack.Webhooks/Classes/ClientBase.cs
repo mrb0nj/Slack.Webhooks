4287cac1a6dab0cded3d4c2c031c4ae9497bedd4
Benn <benn@redrabbits.co.uk>
2020-03-04 20:40:46 +0000

    Fix GetAsync<T> method to correctly set the query data

--- src/Slack.Webhooks/Classes/ClientBase.cs
+++ src/Slack.Webhooks/Classes/ClientBase.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using Slack.Webhooks.Helpers;
 
 namespace Slack.Webhooks.Classes
@@ -32,21 +33,27 @@ namespace Slack.Webhooks.Classes
             }
         }
 
-        protected async Task<T> GetAsync<T>(Uri uri, object payload, bool requireAuthToken = true,
-            bool configureAwait = true) where T : class
+        protected async Task<T> GetAsync<T>(Uri uri, object payload, bool configureAwait = true) where T : class
         {
-            using (var request = new HttpRequestMessage(HttpMethod.Get, uri))
+            var builder = new UriBuilder(uri) {Query = await GetQueryStringAsync(uri, payload)};
+            using (var request = new HttpRequestMessage(HttpMethod.Get, builder.Uri))
             {
-                if (requireAuthToken)
-                    request.Headers.Authorization = AuthenticationHeaderValue.Parse($"Bearer {Configuration.AuthToken}");
-
-                var dictionary = SerializationHelper.ToDictionary(payload);
-                request.Content = new FormUrlEncodedContent(dictionary);
                 var response = await Configuration.HttpClient.SendAsync(request).ConfigureAwait(configureAwait);
                 var content = await response.Content.ReadAsStringAsync().ConfigureAwait(configureAwait);
 
                 return typeof(T) == typeof(string) ? (T)Convert.ChangeType(content, typeof(T)) : SerializationHelper.Deserialize<T>(content);
             }
+        }
+
+        private async Task<string> GetQueryStringAsync(Uri uri, object payload)
+        {
+            var dictionary = SerializationHelper.ToDictionary(payload);
+            dictionary.Add("token", Configuration.AuthToken);
+            
+            var existingQuery = HttpUtility.ParseQueryString(uri.Query);
+            foreach (string key in existingQuery) dictionary.Add(key, existingQuery[key]);
+            
+            return await new FormUrlEncodedContent(dictionary).ReadAsStringAsync();
         }
     }
 }
