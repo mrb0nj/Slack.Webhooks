44f95907967ac6806b98988a6b58bf6a2c14f046
Nicholas Smith <nicholas.smith4@utdallas.edu>
2021-02-08 18:31:32 -0600

    Added Header Block
    https://api.slack.com/reference/block-kit/blocks#header

---
+++ src/Slack.Webhooks/Blocks/Header.cs
@@ -0,0 +1,23 @@
+﻿using Slack.Webhooks.Elements;
+
+namespace Slack.Webhooks.Blocks
+{
+    /// <summary>
+    /// A <see cref="Header"/> is a plain-text block that displays in a larger, bold font.
+    /// Use it to delineate between different groups of content in your app's surfaces.
+    /// </summary>
+    public class Header : Block
+    {
+        /// <summary>
+        /// The <see cref="Text"/> for the block, in the form of a <see cref="TextObject"/>.
+        /// Maximum length for the <c>text</c> in this field is 150 characters.
+        /// </summary>
+        public TextObject Text { get; set; }
+        /// <summary>
+        /// Create a new <see cref="Header"/> instance.
+        /// </summary>
+        public Header() : base(BlockType.Header)
+        {
+        }
+    }
+}
